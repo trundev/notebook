5c7f0db2145c014d07d4923d3d6be52d3fa2d849
Todor Trundev <trundev@gmail.com>
2022-07-11 23:03:30 +0300

    Experimetal from_next_sample()

--- rlc_reverse.py
+++ rlc_reverse.py
@@ -70,6 +70,25 @@ def from_4samples(vals: np.array, dt: float, conjugated: bool=False, squeeze: bo
         phi = phi.real
     return omega, phi
 
+def from_next_sample(vals, exp_omegas, val_next):
+    """Experimental"""
+    exp_omega_next_a = (2 * val_next - exp_omegas[1] * vals[1]) / vals[0]
+    exp_omega_next_b = (2 * val_next - exp_omegas[0] * vals[0]) / vals[1]
+
+    exp_omegas_next = np.array([exp_omega_next_a, exp_omega_next_b])
+    vals_next = exp_omegas_next * vals
+    if np.round(vals_next.mean() - val_next, 12):
+        print(f'Warnig: {vals_next.mean()-val_next=}')
+        ###PATCH
+        vals_next -= vals_next.mean() - val_next
+        #print(f' - patch: {vals_next.mean()-val_next=}')
+        exp_omegas_next = vals_next / vals
+
+    # if some one forgots
+    assert not np.round(vals_next.mean() - val_next, 12).any()
+    assert not np.round(exp_omegas_next - vals_next / vals, 12).any()
+
+    return exp_omegas_next, vals_next
 
 #
 # Test scenarios
@@ -135,37 +154,69 @@ if __name__ == '__main__':
             b0 = b0 * PLUS_MINUS
         test_combined(a + b, a0 + b0, sample_dt, conjugated=add_conj)
 
-    print('=== Simple attenuating oscillation ===')
-    test_combined(complex(-.1, np.pi/2), 0, .25)
-    print('=== Conjugated attenuating oscillation ===')
-    test_separated(
-            -.1, np.pi/2,
-            0, 0,
-            imag_b=True, add_conj=True,
-            sample_dt=.25)
-    print(f'\n=== Range attenuation ===')
-    for omega_r in np.linspace(-5, 5, 10, endpoint=True):
-        test_combined(complex(omega_r, np.pi/2), 0, 1/3)
-    print(f'\n=== Range frequency ===')
-    for omega_i in np.linspace(1e-2, 3*np.pi, 10, endpoint=False):  # dt must be less than 1/2 period
-        test_combined(complex(-.1, omega_i), 0, sample_dt=1/3)
-    # Traverse all imag_b, add_conj combinations: 0-False, 1-True
-    for imag_b, add_conj in np.indices((2, 2)).reshape(2,-1).T:
-        imag_b, add_conj = bool(imag_b), bool(add_conj)
-        title = ', '.join([
-                'oscillating' if imag_b else 'non-oscillating',
-                'conjugated' if add_conj else 'simple'])
-        print(f'\n=== Range attenuation phase:  {title} ===')
-        for phi_r in np.linspace(-5, 5, 10, endpoint=True):
-            test_separated(
-                    -.1, np.pi/2,
-                    phi_r, 0,
-                    imag_b=imag_b, add_conj=add_conj,
-                    sample_dt=.25)
-        print(f'\n=== Range attenuation aux-phase: {title} ===')
-        for phi_i in np.linspace(np.pi, -np.pi, 10, endpoint=False):    # +/-pi are indistinguishable
-            test_separated(
-                    -.1, np.pi/2,
-                    0, phi_i,
-                    imag_b=imag_b, add_conj=add_conj,
-                    sample_dt=.25)
+#    print('=== Simple attenuating oscillation ===')
+#    test_combined(complex(-.1, np.pi/2), 0, .25)
+#    print('=== Conjugated attenuating oscillation ===')
+#    test_separated(
+#            -.1, np.pi/2,
+#            0, 0,
+#            imag_b=True, add_conj=True,
+#            sample_dt=.25)
+#    print(f'\n=== Range attenuation ===')
+#    for omega_r in np.linspace(-5, 5, 10, endpoint=True):
+#        test_combined(complex(omega_r, np.pi/2), 0, 1/3)
+#    print(f'\n=== Range frequency ===')
+#    for omega_i in np.linspace(1e-2, 3*np.pi, 10, endpoint=False):  # dt must be less than 1/2 period
+#        test_combined(complex(-.1, omega_i), 0, sample_dt=1/3)
+#    # Traverse all imag_b, add_conj combinations: 0-False, 1-True
+#    for imag_b, add_conj in np.indices((2, 2)).reshape(2,-1).T:
+#        imag_b, add_conj = bool(imag_b), bool(add_conj)
+#        title = ', '.join([
+#                'oscillating' if imag_b else 'non-oscillating',
+#                'conjugated' if add_conj else 'simple'])
+#        print(f'\n=== Range attenuation phase:  {title} ===')
+#        for phi_r in np.linspace(-5, 5, 10, endpoint=True):
+#            test_separated(
+#                    -.1, np.pi/2,
+#                    phi_r, 0,
+#                    imag_b=imag_b, add_conj=add_conj,
+#                    sample_dt=.25)
+#        print(f'\n=== Range attenuation aux-phase: {title} ===')
+#        for phi_i in np.linspace(np.pi, -np.pi, 10, endpoint=False):    # +/-pi are indistinguishable
+#            test_separated(
+#                    -.1, np.pi/2,
+#                    0, phi_i,
+#                    imag_b=imag_b, add_conj=add_conj,
+#                    sample_dt=.25)
+
+    exp_omega = np.exp(np.array([
+            1j/8*np.pi,
+#            1j/4*np.pi,
+        ]))
+    exp_phi = np.array([
+            1,
+#            3,
+        ])
+
+    fn = exp_phi[:, None] * exp_omega[:, None] ** np.arange(12)
+    print(f'Src vals: {np.round(fn, 3)}')
+    print(f'  angles: {np.angle(fn[:, 1:] / fn[:, :-1], deg=True)}')
+
+    fn_m = fn.mean(0)
+    print(f'fn_m: {np.round(fn_m, 3)}')
+    fn_steps = fn_m[1:] / fn_m[:-1]
+    print(f'  step angles: {np.round(np.angle(fn_steps, deg=True), 1)}')
+    print(f'  step magnitudes: {np.round(np.abs(fn_steps), 3)}')
+
+    ### Use real base-vals
+    base_vals = np.array((fn_m[1], fn_m[1].conj()))
+    base_omega = base_vals / fn_m[0]
+    ###
+
+    vals = fn_m.real[2:]
+    print(f'Start: vals {np.round(base_vals, 3)}, omega {np.round(base_omega, 3)}')
+    for idx, val in enumerate(vals, 2):
+        next_omega, next_vals = from_next_sample(base_vals, base_omega, val)
+        print(f'{idx}: {np.round(val, 3)} -> {np.round(next_vals, 3)}, omega {np.round(np.angle(next_omega, deg=True), 1)} deg, magnitude {np.round(np.abs(next_omega), 3)}')
+        print(f'  expected {np.round(fn_m[idx], 3)}, diff {np.round([fn_m[idx], fn_m[idx].conj()]-next_vals, 3)}')
+        base_omega, base_vals = next_omega, next_vals
